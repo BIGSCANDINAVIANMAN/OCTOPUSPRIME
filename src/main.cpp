27991607195bfff4e5f6e67af6f17d1c7a573fea
Akaera24 <156172097+Akaera24@users.noreply.github.com>
2024-09-26 08:34:29 +0800

    THE BIG UPDATE

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,8 @@ competition Competition;
 /*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+void pre_auton(void)
+{
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
@@ -42,7 +43,8 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void) {
+void autonomous(void)
+{
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
@@ -71,7 +73,7 @@ void autonomous(void) {
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+vex::brain Brain;
 
 // define your global instances of motors and other devices here
 motor frontLeft = motor(19);
@@ -81,39 +83,43 @@ motor backRight = motor(9, true);
 motor_group allWheels = motor_group(frontLeft, frontRight, backLeft, backRight);
 controller Controller1 = controller(primary);
 
-void usercontrol(void) {
-    double rx = Controller1.Axis1.position(percent);
-    double ry = Controller1.Axis2.position(percent);
-    double lx = Controller1.Axis4.position(percent);
-    double ly = Controller1.Axis3.position(percent);
+void usercontrol(void)
+{
+  double rx = Controller1.Axis1.position(percent);
+  double ry = Controller1.Axis2.position(percent);
+  double lx = Controller1.Axis4.position(percent);
+  double ly = Controller1.Axis3.position(percent);
 
-    while(1) {
-        double velocity1 = sqrt(2)/2 * (ry - rx);
-        double velocity2 = sqrt(2)/2 * (ry + rx);
+  while (1)
+  { // hi heny
+    double velocity1 = sqrt(2) / 2 * (ry - rx);
+    double velocity2 = sqrt(2) / 2 * (ry + rx);
 
-        frontLeft.setVelocity(velocity1, percent);
-        backRight.setVelocity(velocity1, percent);
+    frontLeft.setVelocity(velocity1, percent);
+    backRight.setVelocity(velocity1, percent);
 
-        frontRight.setVelocity(velocity2, percent);
-        backLeft.setVelocity(velocity2, percent);
+    frontRight.setVelocity(velocity2, percent);
+    backLeft.setVelocity(velocity2, percent);
 
-        allWheels.spin(forward);
-    }
+    allWheels.spin(forward);
+  }
 }
 
-int main() {
-    while(1) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
-    }
-}
+int main()
+{
+  while (1)
+  {
 
+    // Allow other tasks to run
+    this_thread::sleep_for(10);
+  }
+}
 
 //
 // Main will set up the competition functions and callbacks.
 //
-int main() {
+int main()
+{
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
@@ -122,7 +128,8 @@ int main() {
   pre_auton();
 
   // Prevent main from exiting with an infinite loop.
-  while (true) {
+  while (true)
+  {
     wait(100, msec);
   }
 }
